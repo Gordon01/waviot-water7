da7877d3e9822120f693c359168c4d7333babf62
Alexander <etc9053@gmail.com>
2020-02-25 18:44:54 +0300

    Номер параметра в регулярном сообщении

--- lib/WVT_Water7.c
+++ lib/WVT_Water7.c
@@ -351,16 +351,24 @@ static void WVT_W7_Additional_Parameter(uint16_t address, uint8_t * data)
  * 
  * @returns	    Число записанных байт
  */
-uint8_t WVT_W7_Short_Regular(uint8_t * responce_buffer,
-    uint16_t schedule, 
+uint8_t WVT_W7_Short_Regular(
+    uint8_t * responce_buffer,
     int32_t payload,
+    uint8_t parameter_number,
+    uint16_t schedule, 
     int32_t additional_parameters)
 {
+ 
+    if (parameter_number > 63)
+    {
+        return 0;
+    }
+
     uint8_t parameters[5];
     const uint8_t number_of_additional_params = WVT_W7_Parse_Additional_Parameters(parameters, 
         additional_parameters);
-    
-    responce_buffer[0] = (WVT_W7_REGULAR_MESSAGE_FLAG);
+
+    responce_buffer[0] = (WVT_W7_REGULAR_MESSAGE_FLAG | parameter_number);
     responce_buffer[1] = (schedule >> 8);
     responce_buffer[2] = schedule;
     
--- tests/UT_Water7.cpp
+++ tests/UT_Water7.cpp
@@ -129,13 +129,18 @@ TEST_CASE("Short regular", "[short_regular]")
     //  тип | расписание | значение 4 байта      | адрес доппараметра | его значение 4 байта
         0x80, 0xBE, 0xEF,  0x7A, 0xCE, 0xFE, 0xED, parameter_address,   0x00, 0x00, 0x00, parameter_address };
 
-    CHECK(WVT_W7_Short_Regular(read_buffer, schedule, payload, 0) == packet_length_no_additional);
+    CHECK(WVT_W7_Short_Regular(read_buffer, payload, 0, schedule, 0) == packet_length_no_additional);
 	CHECK(memcmp(short_packet, read_buffer, packet_length_no_additional) == 0);
 
-    CHECK(WVT_W7_Short_Regular(read_buffer, schedule, payload, parameter_address) == packet_length_one_additional);
+    uint8_t parameter_number = 10;
+    short_packet[0] = (parameter_number | 0x80);
+    CHECK(WVT_W7_Short_Regular(read_buffer, payload, parameter_number, schedule, 0) == packet_length_no_additional);
+	CHECK(memcmp(short_packet, read_buffer, packet_length_no_additional) == 0);
+
+    CHECK(WVT_W7_Short_Regular(read_buffer, payload, 0, schedule, parameter_address) == packet_length_one_additional);
 	CHECK(memcmp(short_packet_with_one, read_buffer, packet_length_one_additional) == 0);
 
-    CHECK(WVT_W7_Short_Regular(read_buffer, schedule, payload, five_additional_parameters) == packet_length_five_additional);
+    CHECK(WVT_W7_Short_Regular(read_buffer, payload, 0, schedule, five_additional_parameters) == packet_length_five_additional);
 }
 
 TEST_CASE("Additional parameters", "[additional_parameters]")
